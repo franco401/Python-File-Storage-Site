b6f57e16213841b2440f29870d510a34c2190b5c
franco401 <francojimenez168@gmail.com>
2024-02-28 17:53:48 -0500

    Configured database
    Added database connection and File model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+from django.db.models.functions import Now
+
+class File(models.Model):
+    #base64 encoded id
+    file_id = models.CharField(max_length=64)
+    
+    #name of file (max 255 chars)
+    name = models.CharField()
+
+    #size of file in bytes
+    size = models.IntegerField()
+
+    #when the file was uploaded
+    date_uploaded = models.DateTimeField(db_default=Now()) 
